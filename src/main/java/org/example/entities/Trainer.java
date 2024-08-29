5864ec40cb1d7337298d108b0050452460953066
anca.ditiu <anca_ditiu@yahoo.com>
2024-08-29 20:34:49 +0300

    structura comuna hibernate si pom

---
+++ src/main/java/org/example/entities/Trainer.java
@@ -0,0 +1,23 @@
+package org.example.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Trainer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+    private String lastName;
+
+}
