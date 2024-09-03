2a3022a0ef1c0a8f3e242bfe11b22014f1a73e6c
FlorinCjc <florinncjc@gmail.com>
2024-09-03 16:14:05 +0100

    am adaugat relatia de many to many Member si TrainingSession + hibernate.cfg.xml

--- src/main/java/org/example/entities/Member.java
+++ src/main/java/org/example/entities/Member.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.example.enums.MembershipType;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
@@ -22,8 +23,11 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private MembershipType membershipType;
 
+    @ManyToMany(mappedBy = "members")
+    private List<TrainingSession> trainingSessions;
 
-
+//Many-to-Many între Member și TrainingSession: Un membru poate participa la mai multe sesiuni
+// , iar o sesiune poate avea mai mulți membri înscriș
 
 
 }
