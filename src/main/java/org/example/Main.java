77fe3e550f21b809184730690cf9affb88fde825
FlorinCjc <florinncjc@gmail.com>
2024-09-09 19:47:45 +0100

    main adaugat

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,19 @@
 package org.example;
 
-import org.example.entities.Member;
-import org.example.entities.TrainingSession;
+import org.example.entities.*;
 import org.example.enums.MembershipType;
 import org.example.enums.SessionName;
-import org.example.repositories.MemberRepository;
+import org.example.repositories.*;
 
-import org.example.entities.Trainer;
 import org.example.enums.Specialization;
-import org.example.repositories.TrainerRepository;
-import org.example.repositories.TrainingSessionRepository;
 import org.example.util.HibernateUtil;
-import org.hibernate.LazyInitializationException;
 
-import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+
 
 public class Main {
 
@@ -27,6 +24,9 @@ public class Main {
 
     public static TrainingSession trainingSession = new TrainingSession();
     public static TrainingSessionRepository trainingSessionRepository = new TrainingSessionRepository();
+
+    public static SubscriptionRepository subscriptionRepository = new SubscriptionRepository();
+    public static ProgressRepository progressRepository = new ProgressRepository();
 
     public static void main(String[] args) {
         boolean running = true;
@@ -65,10 +65,18 @@ public class Main {
                     viewTrainingSession();
                     break;
                 case 11:
+                    editTrainingSession();
                     break;
                 case 12:
+                    editSubscription();
                     break;
                 case 13:
+                    viewSubscription();
+                    break;
+                case 14:
+                    renewSubscription();
+                    break;
+                case 15:
                     running = false;
                     break;
                 default:
@@ -90,6 +98,10 @@ public class Main {
         System.out.println("\n8. Delete Member");
         System.out.println("\n9. Add Training Session");
         System.out.println("\n10. View Training Session");
+        System.out.println("\n11. Edit Training Session");
+        System.out.println("\n12. Edit Subscription");
+        System.out.println("\n13. View Subscription");
+        System.out.println("\n14. Renew Subscription");
         System.out.println("\n13. Exit application");
 
         System.out.print("Enter your choice: ");
@@ -141,7 +153,7 @@ public class Main {
         System.out.print("What trainer do you want to edit? (insert id) \n");
         int searchTrainerId = Integer.parseInt(scanner.nextLine());
         Trainer foundTrainer = trainerRepository.findById(searchTrainerId);
-        if(trainerRepository.findById(searchTrainerId) != null){
+        if (trainerRepository.findById(searchTrainerId) != null) {
             System.out.print("What do you want to edit? (first name, last name or spcialization) \n");
             String fieldToUpdate = scanner.nextLine();
             switch (fieldToUpdate) {
@@ -180,7 +192,7 @@ public class Main {
         System.out.print("What member do you want to edit? (insert id) \n");
         int searchMemberId = Integer.parseInt(scanner.nextLine());
         Member foundMember = memberRepository.findById(searchMemberId);
-        if(memberRepository.findById(searchMemberId) != null) {
+        if (memberRepository.findById(searchMemberId) != null) {
             System.out.print("What do you want to edit? (first name, last name or membership) \n");
             String fieldToUpdate = scanner.nextLine();
             switch (fieldToUpdate) {
@@ -228,7 +240,8 @@ public class Main {
         Member foundMember = memberRepository.findById(id);
         memberRepository.delete(foundMember);
     }
-    public static void saveTrainingSession(){
+
+    public static void saveTrainingSession() {
         TrainingSession trainingSession = new TrainingSession();
         System.out.print("Enter Session Name: \n");
         String sessionName = scanner.nextLine();
@@ -244,16 +257,16 @@ public class Main {
         List<Member> memberList = new ArrayList<>();
         System.out.println("Do you want to add member? Yes or No");
         String answer = scanner.nextLine();
-        if("Yes".equals(answer)){
+        if ("Yes".equals(answer)) {
             boolean flag = true;
-            while (flag){
+            while (flag) {
                 System.out.println("What is the id of the member? ");
                 int memberId = Integer.parseInt(scanner.nextLine());
                 Member member = memberRepository.findById(memberId);
                 memberList.add(member);
                 System.out.println("Do you want to add another member? Yes or No");
                 String answer2 = scanner.nextLine();
-                if("No".equals(answer2)){
+                if ("No".equals(answer2)) {
                     flag = false;
                 }
             }
@@ -262,15 +275,15 @@ public class Main {
         trainingSessionRepository.save(trainingSession);
     }
 
-    public static void viewTrainingSession(){
+    public static void viewTrainingSession() {
         System.out.println("What training session do you want to see? (Insert Trainer or Member) ");
         String answer = scanner.nextLine();
-        if("Trainer".equals(answer)){
+        if ("Trainer".equals(answer)) {
             System.out.println("What trainer do you want to see the schedule for? (Insert ID) ");
-             int trainerId = Integer.parseInt(scanner.nextLine());
-             List<TrainingSession> trainingSessionList = trainerRepository.findById(trainerId).getTrainingSessions();
+            int trainerId = Integer.parseInt(scanner.nextLine());
+            List<TrainingSession> trainingSessionList = trainerRepository.findById(trainerId).getTrainingSessions();
             System.out.println("The trainer has the following schedule ");
-            for(TrainingSession tr : trainingSessionList){
+            for (TrainingSession tr : trainingSessionList) {
                 System.out.println("Session " + tr.getSessionName() + " has the following schedule " + tr.getSchedule());
             }
         } else if ("Member".equals(answer)) {
@@ -278,16 +291,95 @@ public class Main {
             int memberId = Integer.parseInt(scanner.nextLine());
             List<TrainingSession> trainingSessions = memberRepository.findById(memberId).getTrainingSessions();
             System.out.println("The member has following schedule ");
-            for(TrainingSession ms : trainingSessions){
+            for (TrainingSession ms : trainingSessions) {
                 System.out.println("Session " + ms.getSessionName() + " has the following schedule " + ms.getSchedule());
             }
         }
     }
-    public static void editTrainingSession(){
+
+    public static void editTrainingSession() {
         System.out.println("What training session do you want to edit? (Insert training session ID )");
         int sessionId = Integer.parseInt(scanner.nextLine());
         TrainingSession trainingSession1 = trainingSessionRepository.findById(sessionId);
     }
 
+    public static void editSubscription() {
+        Subscription subscription = new Subscription();
+        System.out.println("Which member subscription do you want to renew (Insert ID)");
+        int id = Integer.parseInt(scanner.nextLine());
+        Member member = memberRepository.findById(id);
+        if (member != null) {
+            System.out.println("Insert new membership: (MONTHLY , ANNUALLY) \n");
+            String membership = scanner.nextLine();
+            subscription.setMembershipType(MembershipType.valueOf(membership.toUpperCase()));
+            System.out.println("Insert Date when you want to start: (Format YYYY-MM-DD) \n");
+            String date = scanner.nextLine();
+            subscription.setStartDate(LocalDate.parse(date));
+            System.out.println("Insert Date when you want to end subscription: (Format YYYY-MM-DD) \n");
+            String date2 = scanner.nextLine();
+            subscription.setEndDate(LocalDate.parse(date2));
+            member.setMembershipType(subscription.getMembershipType());
+            memberRepository.update(member);
+            subscriptionRepository.save(subscription);
 
+        } else {
+            System.out.println("This member doesn't exists");
+            editSubscription();
+        }
+    }
+
+    public static void viewSubscription() {
+        System.out.println("Enter member id to check his subscription (Insert ID) ");
+        int id = Integer.parseInt(scanner.nextLine());
+        Subscription subscription = memberRepository.findById(id).getSubscription();
+        System.out.println("The membership type is following: ");
+            System.out.println("id" + subscription.getId() + "MembershipType" + subscription.getMembershipType() +
+                    " Members " + subscription.getMembers() + " Start Date: " + subscription.getStartDate() + " End Date:" + subscription.getEndDate());
+        }
+
+
+    public static void renewSubscription() {
+        Subscription subscriptionType = new Subscription();
+        System.out.println("Enter member id for renew his subscription (Insert ID)");
+        int id = Integer.parseInt(scanner.nextLine());
+        Member member = memberRepository.findById(id);
+        if (memberRepository.findById(id) != null) {
+            String fieldToAdd = scanner.nextLine();
+            switch (fieldToAdd) {
+                case "Membership Type": {
+                    System.out.println("Insert new membership type write: (MONTHLY OR ANNUALLY) \n");
+                    String membership = scanner.nextLine();
+                    member.setMembershipType(MembershipType.valueOf(membership.toUpperCase()));
+                    memberRepository.update(member);
+                }
+                break;
+                case "Start Date": {
+//                    System.out.println("Insert Date when you want to start: (Format YYYY,MM,DD) \n");
+//                    String date = scanner.nextLine();
+//                    subscriptionType.setStartDate(new Date(date));
+//                    memberRepository.update(member);
+                }
+                break;
+                case "End Date": {
+//                    System.out.println("Insert Date when you want to end subscription: (Format YYYY,MM,DD) \n");
+//                    String date = scanner.nextLine();
+//                    subscriptionType.setEndDate(new Date(date));
+//                    memberRepository.update(member);
+                }
+                break;
+                default:
+                    System.out.println("You didn't choose an option or you wrote it wrong");
+                    break;
+            }
+        } else {
+            System.out.println("This member doesn't exists");
+            renewSubscription();
+        }
+    }
+    public static void deleteSubscription(){
+        System.out.println("Insert member do you want to delete subscription: (Insert ID) \n");
+        int id = Integer.parseInt(scanner.nextLine());
+        Member foundMember = memberRepository.findById(id);
+        subscriptionRepository.delete(foundMember);
+    }
 }
